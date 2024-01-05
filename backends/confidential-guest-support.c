29f6718b45cca37261ad29441debd054bd499346
Roy Hopkins <roy.hopkins@suse.com>
2024-01-05 11:37:52 +0000

    backends/confidential-guest-support: Add virtual functions to support IGVM
    In preparation for supporting the processing of IGVM files to configure
    guests, this adds a set of functions to ConfidentialGuestSupport
    allowing configuration of secure virtual machines that can be
    implemented for each supported isolation platform type such as Intel TDX
    or AMD SEV-SNP. These functions will be called by IGVM processing code
    in subsequent patch.
    
    This commit provides a default implementation of the functions that
    either perform no action or generate a warning or error when they are
    called. Targets that support ConfidentalGuestSupport should override
    these implementations.
    
    Signed-off-by: Roy Hopkins <roy.hopkins@suse.com>

--- backends/confidential-guest-support.c
+++ backends/confidential-guest-support.c
@@ -14,6 +14,7 @@
 #include "qemu/osdep.h"
 
 #include "exec/confidential-guest-support.h"
+#include "qemu/error-report.h"
 
 OBJECT_DEFINE_ABSTRACT_TYPE(ConfidentialGuestSupport,
                             confidential_guest_support,
@@ -45,8 +46,47 @@ static void confidential_guest_support_class_init(ObjectClass *oc, void *data)
 #endif
 }
 
+static int check_support(ConfidentialGuestPlatformType platform,
+                         uint16_t platform_version, uint8_t highest_vtl,
+                         uint64_t shared_gpa_boundary)
+{
+    /* Default: no support. */
+    return 0;
+}
+
+static int set_memory_attributes(hwaddr gpa, uint8_t *ptr, uint64_t len,
+                                 int memory_type)
+{
+    warn_report("Confidential guest memory attributes not supported");
+    return -1;
+}
+
+static int set_cpu_context(uint16_t cpu_index, const void *ctx,
+                           uint32_t ctx_len)
+{
+    warn_report("Confidential guest CPU context not supported");
+    return -1;
+}
+
+static int get_mem_map_count(void)
+{
+    return 0;
+}
+
+static void get_mem_map_entry(int index, ConfidentialGuestMemoryMapEntry *entry)
+{
+    error_report("Invalid memory map entry requested");
+    exit(EXIT_FAILURE);
+}
+
 static void confidential_guest_support_init(Object *obj)
 {
+    ConfidentialGuestSupport *cgs = CONFIDENTIAL_GUEST_SUPPORT(obj);
+    cgs->check_support = check_support;
+    cgs->set_memory_attributes = set_memory_attributes;
+    cgs->set_cpu_context = set_cpu_context;
+    cgs->get_mem_map_count = get_mem_map_count;
+    cgs->get_mem_map_entry = get_mem_map_entry;
 }
 
 static void confidential_guest_support_finalize(Object *obj)
