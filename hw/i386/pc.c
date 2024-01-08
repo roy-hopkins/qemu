881582e2020d5f3f1f6dd25376290d42b2ed96c0
Roy Hopkins <roy.hopkins@suse.com>
2024-01-08 10:36:07 +0000

    i386/pc: Skip initialization of system FW when using IGVM
    When using an IGVM file the configuration of the system firmware is
    defined by IGVM directives contained in the file. Therefore the default
    system firmware should not be initialized when an IGVM file has been
    provided.
    
    This commit checks to see if an IGVM file has been provided and, if it
    has then the standard system firmware initialization is skipped and any
    prepared flash devices are cleaned up.
    
    Signed-off-by: Roy Hopkins <roy.hopkins@suse.com>

--- hw/i386/pc.c
+++ hw/i386/pc.c
@@ -63,6 +63,7 @@
 #include "e820_memory_layout.h"
 #include "trace.h"
 #include CONFIG_DEVICES
+#include "exec/confidential-guest-support.h"
 
 #ifdef CONFIG_XEN_EMU
 #include "hw/xen/xen-legacy-backend.h"
@@ -1033,8 +1034,22 @@ void pc_memory_init(PCMachineState *pcms,
         }
     }
 
-    /* Initialize PC system firmware */
-    pc_system_firmware_init(pcms, rom_memory);
+    /* 
+     * If this is a confidential guest configured using IGVM then the IGVM 
+     * configuration will include the system firmware. In this case do not 
+     * initialise PC system firmware.
+     */
+    if (!cgs_is_igvm(machine->cgs)) {
+        /* Initialize PC system firmware */
+        pc_system_firmware_init(pcms, rom_memory);
+    }
+    else {
+        /*
+         * The flash devices were created when the PC was initialized. We can clean
+         * the devices up here as they will not be used.
+         */ 
+         pc_system_flash_cleanup_unused(pcms);
+    }
 
     option_rom_mr = g_malloc(sizeof(*option_rom_mr));
     memory_region_init_ram_guest_memfd(option_rom_mr, NULL, "pc.rom", PC_ROM_SIZE,
